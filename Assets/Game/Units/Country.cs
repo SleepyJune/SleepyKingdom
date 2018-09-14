fcf0ff77b7ba53c0ddee22b027b3b1b1a369c86e
notequals <hshsiao63@gmail.com>
2018-09-13 17:38:18 -0700

    Initial Commit

---
+++ Assets/Game/Units/Country.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Country : MonoBehaviour
+{
+    public string countryName;
+
+    //leader
+
+    public int population;
+
+    public int wood;
+    public int stone;
+    public int wheat;
+    public int gold;
+}
---
+++ Assets/Game/Units/County.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class County : MonoBehaviour
+{
+    public string countryName;
+
+    //leader
+
+    public List<Hero> deputies;
+
+    public int population;
+
+    public int wood;
+    public int stone;
+    public int wheat;
+    public int gold;
+
+    public float taxRate;
+
+    public int happiness;
+}
